9200afcefcedd19daa1bb164957e7b162a9c10fa
hiki31376 <135303047+hiki31376@users.noreply.github.com>
2023-06-07 10:38:54 +0900

    Quick sort (#7)
    * 퀵 정렬 추가 sort interpace에 default 메서드 추가
    
    * 저장 잘못됨
    
    * 오류 발생

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -4,44 +4,36 @@ import java.util.List;
 
 public class QuickSort implements SortAlgoritm{
 
-    public void quickSort(int[] array, int left, int right) {
-        if(left >= right) return;
-        
-        // 분할 
-        int pivot = partition(); 
-        
-        // 피벗은 제외한 2개의 부분 배열을 대상으로 순환 호출
-        quickSort(array, left, pivot-1);  // 정복(Conquer)
-        quickSort(array, pivot+1, right); // 정복(Conquer)
+    @Override
+    public List<Integer> sort(List<Integer> arr) {
+        quickSort(arr, 0, arr.size()-1);
+        return arr;
     }
-    
-    public int partition(int[] array, int left, int right) {
-        /**
-        // 최악의 경우, 개선 방법
-        int mid = (left + right) / 2;
-        swap(array, left, mid);
-        */
-        
-        int pivot = array[left]; // 가장 왼쪽값을 피벗으로 설정
+    public void quickSort(List<Integer> arr , int left , int right){
+        if(left >= right) return;
+
+        int pivot = partition(arr, left, right);
+
+        quickSort(arr, left, pivot-1);
+        quickSort(arr, pivot+1, right);
+    }
+
+    public int partition(List<Integer> arr , int left , int right){
+        int pivot = arr.get(left); // 가장 왼쪽값을 피벗으로 설정
         int i = left, j = right;
         
         while(i < j) {
-            while(pivot < array[j]) {
+            while(pivot < arr.get(j)) {
                 j--;
             }
-            while(i < j && pivot >= array[i]){
+            while(i < j && pivot >= arr.get(i)){
                 i++;
             }
-            swap(array, i, j);
+            swap(arr, i, j);
         }
-        array[left] = array[i];
-        array[i] = pivot;
+        arr.set(left, i);
+        arr.set(i, pivot);
         
         return i;
-    }
-    @Override
-    public List<Integer> sort(List<Integer> arr) {
-        // TODO Auto-generated method stub
-        return null;
     }
 }
--- src/sort/SortAlgoritm.java
+++ src/sort/SortAlgoritm.java
@@ -4,4 +4,12 @@ import java.util.List;
 
 public interface SortAlgoritm{
     public abstract List<Integer> sort( List<Integer> arr);
+
+    default void swap(List<Integer> arr , int i , int j){
+        int temp;
+        temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j,temp);
+    }
+
 }
